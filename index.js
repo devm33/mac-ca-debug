73954fbf148845a4465617bb99bddf066c3753e4
Devraj Mehta <devm33@github.com>
2023-12-06 10:07:27 -0500

    Add script to list certs

---
+++ index.js
@@ -0,0 +1,6 @@
+const macCa = require('@roamhq/mac-ca');
+const certs = macCa.all(macCa.der2.pem).filter(c => c !== undefined);
+for (const cert of certs) {
+  console.log(cert);
+}
+console.log(`Read ${certs.length} certificates from Mac keychain`);
